08090b302c13fd9382ec8abe0d40a7aadc70911b
Nazia Habib <habib.nazia@gmail.com>
2021-05-16 16:35:53 -0500

    Update QL-SARSA.py

--- Chapter05/QL-SARSA.py
+++ Chapter05/QL-SARSA.py
@@ -25,6 +25,42 @@ for episode in range(episodes):
         totalReward += reward
 
     totalSteps += steps
+
+print("Average timesteps taken: {}".format(totalSteps/episodes))
+print("Average reward: {}".format(totalReward/episodes))
+print("Total reward: {}".format(totalReward))
+
+env = gym.make('Taxi-v2')
+agent = agents.SarsaAgent(states=env.observation_space.n, actions=env.action_space.n)
+
+episodes = 10000
+totalSteps = 0
+totalReward = 0
+
+for episode in range(episodes):
+    
+    state = env.reset()
+    reward = 0
+    steps = 0
+    done = False
+    
+    while not done:
+        
+        action = agent.act(state)
+        if agent.lastAction is not None:
+            agent.learn(agent.lastState, agent.lastAction, agent.lastReward, state, action)
+            
+        agent.lastAction = action
+        agent.lastState = state
+        
+        state, reward, done, info = env.step(action)
+        
+        agent.lastReward = reward
+        totalReward += reward
+
+        steps += 1
+    totalSteps += steps
+
 print("Average timesteps taken: {}".format(totalSteps/episodes))
 print("Average reward: {}".format(totalReward/episodes))
 print("Total reward: {}".format(totalReward))
